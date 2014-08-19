f60b6877e885a5bde0a93617b1ad11b75953d95b
Marko Heijnen <info@markoheijnen.com>
2014-08-19 14:20:49 +0200

    Update to latest Babble version and other fixes.
    - Fixed incorrect usage of interface_lang and content_lang
    - Code cleanup

--- locale.php
+++ locale.php
@@ -3,16 +3,16 @@
 class Babble_Top_Level_Domain_Locale extends Babble_Locale {
 
 	public function __construct( $previous ) {
-		remove_action( 'admin_init', array( & $previous, 'admin_init' ) );
-		remove_action( 'admin_notices', array( & $previous, 'admin_notices' ) );
-		remove_action( 'parse_request', array( & $previous, 'parse_request_early' ), 0 );
-		remove_action( 'pre_comment_on_post', array( & $previous, 'pre_comment_on_post' ) );
-		remove_filter( 'body_class', array( & $previous, 'body_class' ) );
-		remove_filter( 'locale', array( & $previous, 'set_locale' ) );
-		remove_filter( 'mod_rewrite_rules', array( & $previous, 'mod_rewrite_rules' ) );
-		remove_filter( 'post_class', array( & $previous, 'post_class' ), null, 3 );
-		remove_filter( 'pre_update_option_rewrite_rules', array( & $previous, 'internal_rewrite_rules_filter' ) );
-		remove_filter( 'query_vars', array( & $previous, 'query_vars' ) );
+		remove_action( 'admin_init', array( $previous, 'admin_init' ) );
+		remove_action( 'admin_notices', array( $previous, 'admin_notices' ) );
+		remove_action( 'parse_request', array( $previous, 'parse_request_early' ), 0 );
+		remove_action( 'pre_comment_on_post', array( $previous, 'pre_comment_on_post' ) );
+		remove_filter( 'body_class', array( $previous, 'body_class' ) );
+		remove_filter( 'locale', array( $previous, 'set_locale' ) );
+		remove_filter( 'mod_rewrite_rules', array( $previous, 'mod_rewrite_rules' ) );
+		remove_filter( 'post_class', array( $previous, 'post_class' ), null, 3 );
+		remove_filter( 'pre_update_option_rewrite_rules', array( $previous, 'internal_rewrite_rules_filter' ) );
+		remove_filter( 'query_vars', array( $previous, 'query_vars' ) );
 
 		parent::__construct();
 	}
@@ -25,10 +25,9 @@ class Babble_Top_Level_Domain_Locale extends Babble_Locale {
 	 * @param object $wp The WP object, passed by reference (so no need to return)
 	 * @return void
 	 **/
-	public function parse_request_early( $wp ) {
-
+	public function parse_request_early( WP $wp ) {
 		// Otherwise, simply set the lang for this request
-		$wp->query_vars[ 'lang' ] = $this->lang;
+		$wp->query_vars[ 'lang' ] = $this->content_lang;
 		$wp->query_vars[ 'lang_url_prefix' ] = $this->url_prefix;
 	}
 
@@ -44,35 +43,65 @@ class Babble_Top_Level_Domain_Locale extends Babble_Locale {
 		// Deal with the special case of wp-comments-post.php
 		if ( false !== stristr( $_SERVER[ 'REQUEST_URI' ], 'wp-comments-post.php' ) ) {
 			if ( $comment_post_ID = ( isset( $_POST[ 'comment_post_ID' ] ) ) ? (int) $_POST[ 'comment_post_ID' ] : false ) {
-				$this->set_lang( bbl_get_post_lang_code( $comment_post_ID ) );
-				return $this->lang;
+				$this->set_content_lang( bbl_get_post_lang_code( $comment_post_ID ) );
+				return $this->content_lang;
 			}
 		}
 		
-		if ( isset( $this->lang ) )
-			return $this->lang;
+		if ( is_admin() ) {
+			if ( isset( $this->interface_lang ) )
+				return $this->interface_lang;
+		} else {
+			if ( isset( $this->content_lang ) )
+				return $this->content_lang;
+		}
+
 
 		if ( is_admin() ) {
 			// @FIXME: At this point a mischievous XSS "attack" could set a user's admin area language for them
-			if ( isset( $_GET[ 'lang' ] ) ) {
-				$this->set_lang( $_GET[ 'lang' ] );
+			if ( isset( $_POST[ 'interface_lang' ] ) ) {
+				$this->set_interface_lang( $_POST[ 'interface_lang' ] );
 			} else {
 				// $current_user = wp_get_current_user();
-				if ( $lang = $this->get_cookie_lang() )
-					$this->set_lang( $lang );
+				if ( $lang = $this->get_cookie_interface_lang() ) {
+					$this->set_interface_lang( $lang );
+				}
+			}
+			// @FIXME: At this point a mischievous XSS "attack" could set a user's content language for them
+			if ( isset( $_GET[ 'lang' ] ) ) {
+				$this->set_content_lang( $_GET[ 'lang' ] );
+			} else {
+				// $current_user = wp_get_current_user();
+				if ( $lang = $this->get_cookie_content_lang() ) {
+					$this->set_content_lang( $lang );
+				}
 			}
 		} 
 		else { // Front end
 			$lang_code = array_search( $_SERVER['HTTP_HOST'], Babble_Top_Level_Domain::$languages );
 
-			if( $lang_code )
-				$this->set_lang_from_prefix( $lang_code );
+			if( $lang_code ) {
+				$this->set_content_lang_from_prefix( $lang_code );
+			}
+
+			if ( $lang = $this->get_cookie_content_lang() ) {
+				$this->set_interface_lang( $lang );
+			}
 		}
 
-		if ( ! isset( $this->lang ) || ! $this->lang )
-			$this->set_lang( bbl_get_default_lang_code() );
 
-		return $this->lang;
+		if ( ! isset( $this->content_lang ) || ! $this->content_lang ) {
+			$this->set_content_lang( bbl_get_default_lang_code() );
+		}
+
+		if ( ! isset( $this->interface_lang ) || ! $this->interface_lang ) {
+			$this->set_interface_lang( bbl_get_default_lang_code() );
+		}
+
+		if ( is_admin() )
+			return $this->interface_lang;
+		else
+			return $this->content_lang;
 	}
 
 
@@ -94,20 +123,22 @@ class Babble_Top_Level_Domain_Locale extends Babble_Locale {
 			$lang = bbl_get_current_lang();
 			$url  = Babble_Top_Level_Domain::$languages[ $lang->url_prefix ];
 
-			if ( is_ssl() && ! is_admin() )
+			if ( is_ssl() && ! is_admin() ) {
 				$url = 'https://' . $url;
-			else
+			}
+			else {
 				$url = 'http://' . $url;
+			}
 
-			if ( $path && is_string( $path ) )
+			if ( $path && is_string( $path ) ) {
 				$url .= '/' . ltrim( $path, '/' );
+			}
 		}
 
 		return $url;
 	}
 
 	public function internal_rewrite_rules_filter( $rules ) {
-
 		return $rules;
 	}
 
